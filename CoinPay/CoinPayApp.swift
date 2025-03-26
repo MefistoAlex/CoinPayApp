fd5e81b9bcf6ca1fd9182800044b8e9451f957f4
Alex Mefisto <alexandr.mefisto@poly-tech.com.ua>
2025-03-26 11:21:26 +0200

    Initial Commit

---
+++ CoinPay/CoinPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinPayApp.swift
+//  CoinPay
+//
+//  Created by Alexandr Mefisto on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinPayTests/CoinPayTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinPayTests.swift
+//  CoinPayTests
+//
+//  Created by Alexandr Mefisto on 26.03.2025.
+//
+
+import Testing
+@testable import CoinPay
+
+struct CoinPayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
